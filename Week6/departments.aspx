70f73ccd799a534dc8ed8e1b03221293febf9acf
Esat IBIS <esat.taha.ibis@gmail.com>
2016-10-11 09:56:22 -0400

    Database added with Entity Framework and departments page created

---
+++ Week6/departments.aspx
@@ -0,0 +1,13 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="departments.aspx.cs" Inherits="Week6.departments" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <h1>Departments</h1>
+    <asp:gridview ID="grdDepartments" runat="server" CssClass="table table-striped" autogeneratecolumns="false">
+        <Columns>
+            <asp:BoundField dataField="DepartmentID" HeaderText="ID" />
+            <asp:BoundField dataField="Name" HeaderText="Department Name" />
+            <asp:BoundField dataField="Budget" HeaderText="Budget" DataFormatString="{0:c}" />
+        </Columns>
+    </asp:gridview>
+</asp:Content>
---
+++ Week6/departments.aspx.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+// add 2 extra references
+using System.Web.ModelBinding;
+
+namespace Week6
+{
+    public partial class departments : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            // Get the departments and display in the gridView
+            getDepartments();
+        }
+
+        protected void getDepartments()
+        {
+            // connect to the database 
+            var connection = new Entities();
+
+            // run the query using LINQ
+            var Departments = from d in connection.Departments
+                              select d;
+
+            // display the result in gridView
+            grdDepartments.DataSource = Departments.ToList();
+            grdDepartments.DataBind();
+
+        }
+    }
+}
